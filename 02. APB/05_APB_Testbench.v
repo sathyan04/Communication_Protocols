d6ce45175ff7960fe2c51eed515c65ec73ae316e
Sathyan S S <sssathyan2004@gmail.com>
2026-02-17 11:46:43 +0530

    Update 05_APB_Testbench.v

--- 02. APB/05_APB_Testbench.v
+++ 02. APB/05_APB_Testbench.v
@@ -1,30 +1,26 @@
-// Code your testbench here
-// or browse Examples
-
 `timescale 1ns/1ns
 
-module apb_tb;
-  
+module apb_tb;  
   reg PCLK,PRESETn,READ_WRITE,transfer;
   reg [7:0]apb_write_data;
   reg [8:0]apb_write_paddr,apb_read_paddr;
-  
   wire [7:0]apb_read_data_out;
   
-  apb_top dut_top(PCLK,
-                  PRESETn,
-                  READ_WRITE,
-                  transfer,
-                  apb_write_data,
-                  apb_write_paddr,
-                  apb_read_paddr,
-                  apb_read_data_out );
+  apb_top dut_top(
+    PCLK,
+    PRESETn,
+    READ_WRITE,
+    transfer,
+    apb_write_data,
+    apb_write_paddr,
+    apb_read_paddr,
+    apb_read_data_out 
+  );
   
   initial begin
     PCLK = 0;
     forever #5 PCLK = ~PCLK;
   end
-  
   
   task apb_write(input[8:0] addr, input[7:0] data);
     begin
@@ -41,8 +37,6 @@ module apb_tb;
     end
   endtask
   
-  //APB READ TASK
-  
   task apb_read(input[8:0] addr);
     begin
       @(posedge PCLK)
@@ -56,16 +50,12 @@ module apb_tb;
       @(posedge PCLK)
       
       transfer = 0;
-      #1; // Delay for display
+      #1;
 
-      
       $display("apb read_data_out = %0d",apb_read_data_out);
       
     end
   endtask
-  
-      
-    
       
   initial begin
     
@@ -83,15 +73,11 @@ module apb_tb;
     
     PRESETn = 1;
     
-    
     apb_write(9'd5, 8'd55);
     apb_write(9'd10, 8'd99);
-    
-    
 
     apb_write(9'd260, 8'd123); 
     apb_write(9'd300, 8'd200);
-    
     
     apb_read(9'd5);
     apb_read(9'd50);
