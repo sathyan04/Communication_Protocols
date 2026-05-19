48af18cb2a1379c6eae3062f4240df5a7782d4f7
Sathyan S S <sssathyan2004@gmail.com>
2026-05-19 16:01:44 +0530

    Update 02_I2C_controller.sv

--- 02. Off-Chip Protocols/03. I2C (Inter-Intergrated Circuit)/02_I2C_controller.sv
+++ 02. Off-Chip Protocols/03. I2C (Inter-Intergrated Circuit)/02_I2C_controller.sv
@@ -1,220 +1,220 @@
-  module i2c_controller #(
-    parameter CLK_FREQ = 50_000_000,
-    parameter I2C_FREQ = 100_000
-  )(
-    input  wire       clk,
-    input  wire       rst_n,
+module i2c_controller #(
+  parameter CLK_FREQ = 50_000_000,
+  parameter I2C_FREQ = 100_000
+)(
+  input  wire       clk,
+  input  wire       rst_n,
 
-    input  wire       start,
-    input  wire       rw,
-    input  wire [6:0] addr,
-    input  wire [7:0] data_in,
+  input  wire       start,
+  input  wire       rw,
+  input  wire [6:0] addr,
+  input  wire [7:0] data_in,
 
-    output reg  [7:0] data_out,
-    output reg        busy,
-    output reg        done,
-    output reg        ack_error,
+  output reg  [7:0] data_out,
+  output reg        busy,
+  output reg        done,
+  output reg        ack_error,
 
-    inout  wire       sda,	// Bi-directional Serial Data Line
-    output wire		scl
-  );
+  inout  wire       sda,	// Bi-directional Serial Data Line
+  output wire		scl
+);
 
-    localparam DIVIDER = CLK_FREQ / (I2C_FREQ * 2);
-    reg [$clog2(DIVIDER)-1:0] clk_cnt;
-    reg scl_tick;
-    reg scl_prev;
+  localparam DIVIDER = CLK_FREQ / (I2C_FREQ * 2);
+  reg [$clog2(DIVIDER)-1:0] clk_cnt;
+  reg scl_tick;
+  reg scl_prev;
 
-    wire scl_rising;
-    wire scl_falling;
+  wire scl_rising;
+  wire scl_falling;
 
-    reg sda_en;
-    reg scl_en;
-    reg ack_sent;
+  reg sda_en;
+  reg scl_en;
+  reg ack_sent;
 
-    always @(posedge clk or negedge rst_n) begin
-      if (!rst_n) begin
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n) begin
+      clk_cnt  <= 0;
+      scl_tick <= 1;
+    end
+    else begin
+      if (clk_cnt == DIVIDER-1) begin
         clk_cnt  <= 0;
-        scl_tick <= 1;
+        scl_tick <= ~scl_tick;
       end
       else begin
-        if (clk_cnt == DIVIDER-1) begin
-          clk_cnt  <= 0;
-          scl_tick <= ~scl_tick;
-        end
-        else begin
-          clk_cnt  <= clk_cnt + 1'b1;
-        end
+        clk_cnt  <= clk_cnt + 1'b1;
       end
     end
+  end
 
-    always @(posedge clk or negedge rst_n) begin
-      if (!rst_n)
-        scl_prev <= 0;
-      else
-        scl_prev <= scl_tick;
+  always @(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+      scl_prev <= 0;
+    else
+      scl_prev <= scl_tick;
+  end
+
+  assign scl_rising		= (~scl_prev && scl_tick);
+  assign scl_falling	= (scl_prev && ~scl_tick);
+
+  assign sda = sda_en ? 1'b0 : 1'bz;
+  assign scl = (scl_en && !scl_tick) ? 1'b0 : 1'bz;
+
+  reg [2:0] state;
+
+  reg [7:0] shift_reg;
+  reg [2:0] bit_count;
+  reg ack_received;
+
+  localparam idle 			= 0;
+  localparam start_state 	= 1;
+  localparam address 		= 2;
+  localparam ack1 			= 3;
+  localparam write 			= 4;
+  localparam read 			= 5;
+  localparam ack2 			= 6;
+  localparam stop 			= 7;
+
+  always @(posedge clk or negedge rst_n) begin
+    if(!rst_n) begin
+      busy		<= 0;
+      done		<= 0;
+      ack_error	<= 0;
+      data_out	<= 0;
+      sda_en	<= 0;
+      scl_en	<= 0;
+      shift_reg	<= 0;
+      bit_count	<= 0;
+      ack_sent	<= 0;
+      state		<= idle;
     end
+    else begin
+      case(state)
 
-    assign scl_rising		= (~scl_prev && scl_tick);
-    assign scl_falling	= (scl_prev && ~scl_tick);
+        idle:begin
+          busy		<= 0;
+          done		<= 0;
+          ack_error	<= 0;
+          data_out	<= 0;
+          sda_en	<= 0;
+          scl_en	<= 0;
+          ack_sent	<= 0;
+          shift_reg	<= 0;
+          bit_count	<= 0;
+          if(start) begin
+            busy	<= 1;
+            state	<= start_state;
+          end
+          else begin
+            busy	<= 0;
+            state	<= idle;
+          end
+        end
 
-    assign sda = sda_en ? 1'b0 : 1'bz;
-    assign scl = (scl_en && !scl_tick) ? 1'b0 : 1'bz; // SCL will be zero only when scl_en is high and scl_tick is low
+        start_state:begin
+          sda_en	<= 1;
+          scl_en	<= 1;
+          shift_reg	<= {addr,rw};
+          bit_count	<= 0;
+          state		<= address;
+        end
 
-    reg [2:0] state;
-
-    reg [7:0] shift_reg;
-    reg [2:0] bit_count;
-    reg ack_received;
-
-    localparam idle 			= 0;
-    localparam start_state 	= 1;
-    localparam address 		= 2;
-    localparam ack1 			= 3;
-    localparam write 			= 4;
-    localparam read 			= 5;
-    localparam ack2 			= 6;
-    localparam stop 			= 7;
-
-    always @(posedge clk or negedge rst_n) begin
-      if(!rst_n) begin
-        busy		<= 0;
-        done		<= 0;
-        ack_error	<= 0;
-        data_out	<= 0;
-        sda_en	<= 0;
-        scl_en	<= 0;
-        shift_reg	<= 0;
-        bit_count	<= 0;-
-        ack_sent	<= 0;
-        state		<= idle;
-      end
-      else begin
-        case(state)
-
-          idle:begin
-            busy		<= 0;
-            done		<= 0;
-            ack_error	<= 0;
-            data_out	<= 0;
-            sda_en	<= 0;
-            scl_en	<= 0;
-            ack_sent	<= 0;
-            shift_reg	<= 0;
-            bit_count	<= 0;
-            if(start) begin
-              busy	<= 1;
-              state	<= start_state;
+        address:begin
+          if(scl_falling) begin
+            shift_reg	<= {shift_reg[6:0],1'b0};
+            sda_en	<= (shift_reg[7]==0);
+            if(bit_count == 7) begin
+              bit_count	<= 0;
+              state		<= ack1;
             end
             else begin
-              busy	<= 0;
-              state	<= idle;
+              bit_count	<= bit_count + 1'b1;
+              state		<= address;
             end
           end
+        end
 
-          start_state:begin
-            sda_en	<= 1;
-            scl_en	<= 1;
-            shift_reg	<= {addr,rw};
-            bit_count	<= 0;
-            state		<= address;
-          end
-
-          address:begin
-            if(scl_falling) begin
-              shift_reg	<= {shift_reg[6:0],1'b0};
-              sda_en	<= (shift_reg[7]==0);
-              if(bit_count == 7) begin
-                bit_count	<= 0;
-                state		<= ack1;
-              end
+        ack1:begin
+          if (scl_falling) begin
+            ack_received <= sda;
+            if (ack_received == 1) begin
+              ack_error <= 1;
+              state <= stop;
+            end else begin
+              bit_count <= 0;
+              if (rw) state <= read;
               else begin
-                bit_count	<= bit_count + 1'b1;
-                state		<= address;
+                shift_reg <= data_in;
+                state <= write;
               end
             end
           end
+        end
 
-          ack1:begin
-            if (scl_falling) begin
+        write:begin
+          if(scl_falling) begin
+            shift_reg	<= {shift_reg[6:0],1'b0};
+            sda_en		<= (shift_reg[7]==0);
+            if(bit_count == 7) begin
+              bit_count	<= 0;
+              sda_en	<= 0;
+              state		<= ack2;
+            end
+            else begin
+              bit_count	<= bit_count + 1'b1;
+              state		<= write;
+            end
+          end
+        end
+
+        read:begin
+          if(scl_rising)begin
+            shift_reg	<= {shift_reg[6:0],sda};
+            if(bit_count == 7) begin
+              data_out	<= {shift_reg[6:0],sda};
+              bit_count	<= 0;
+              state		<= ack2;
+            end
+            else begin
+              bit_count	<= bit_count + 1'b1;
+              state		<= read;
+            end
+          end
+        end
+
+        ack2:begin
+          if(rw)begin
+            sda_en <= 0;
+            if(scl_falling)
+              state <= stop;
+          end
+
+          else begin
+            if(scl_rising) begin
               ack_received	<= sda;
-              if (ack_received == 1) begin
-                ack_error		<= 1;
-                state			<= stop;
-              end 
-              else begin
-                bit_count		<= 0;
-                if (rw) state	<= read;
-                else begin
-                  shift_reg	<= data_in;
-                  state		<= write;
-                end
-              end
             end
-          end
-
-          write:begin
             if(scl_falling) begin
-              shift_reg	<= {shift_reg[6:0],1'b0};
-              sda_en		<= (shift_reg[7]==0);
-              if(bit_count == 7) begin
-                bit_count	<= 0;
-                sda_en	<= 0;
-                state		<= ack2;
-              end   
-              else begin
-                bit_count	<= bit_count + 1'b1;
-                state		<= write;
-              end
+              state			<= stop;
+              if(ack_received == 1)
+                ack_error	<= 1;
             end
           end
 
-          read:begin
-            if(scl_rising)begin
-              shift_reg	<= {shift_reg[6:0],sda};
-              if(bit_count == 7) begin
-                data_out	<= {shift_reg[6:0],sda};
-                bit_count	<= 0;
-                state		<= ack2;
-              end
-              else begin
-                bit_count	<= bit_count + 1'b1;
-                state		<= read;
-              end
-            end
+        end
+
+        stop:begin
+          sda_en	<= 0;
+          if(scl_tick)begin
+            scl_en	<= 0;
+            busy	<= 0;
+            done	<= 1;
+            state	<= idle;
           end
+        end
 
-          ack2:begin
-            if(rw)begin				// Slave Write - Master Read
-              sda_en		<= 0;		// release SDA (NACK)
-              if(scl_falling)
-                state		<= stop;
-            end
-            else begin				// Master Write - Slave Read
-              if(scl_rising) begin
-                ack_received	<= sda;
-              end
-              if(scl_falling) begin
-                state			<= stop;
-                if(ack_received == 1)	// NACK
-                  ack_error	<= 1;
-              end
-            end
+        default: state<=idle;
 
-          end
-
-          stop:begin
-            sda_en	<= 0;
-            if(scl_tick)begin
-              scl_en	<= 0;
-              busy	<= 0;
-              done	<= 1;
-              state	<= idle;
-            end
-          end
-
-          default: state<=idle;
-
-        endcase
-      end
+      endcase
     end
-  endmodule
+  end
+endmodule
